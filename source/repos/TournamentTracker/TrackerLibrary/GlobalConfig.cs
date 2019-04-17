e44d0bc775202b4e593e4f15cbd4c9cc175214fb
swulks <35363999+swulks@users.noreply.github.com>
2019-04-17 11:27:28 +0300

    17.04.2019

--- source/repos/TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ source/repos/TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static List<IDataConnection> Connections { get; private set; } 
+            = new List<IDataConnection>();
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
@@ -21,7 +23,7 @@ namespace TrackerLibrary
             if (textFiles)
             {
                 //TODO Create the text Connection
-                TextConnection text = new TextConnection();
+                TextConnector text = new TextConnector();
                 Connections.Add(text);               
             }
         }
--- source/repos/TournamentTracker/TrackerLibrary/Models/PersonModel.cs
+++ source/repos/TournamentTracker/TrackerLibrary/Models/PersonModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace TrackerLibrary
+namespace TrackerLibrary.Models
 {
    public class PersonModel
     {
